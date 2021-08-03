1187c18f0d206196e667a2d3c564ff75a2725b0a
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-08-03 11:58:00 -0300

    Finished CRUD

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -9,5 +9,56 @@ export async function createEmployee({
   role,
   salary,
 }) {
-  await Employee.create({ name, birthDate, gender, state, city, role, salary });
+  await Employee.create({
+    name,
+    birthDate,
+    gender,
+    state,
+    city,
+    role,
+    salary,
+  }).catch(() => {
+    throw new Error("Failed to create the Employee");
+  });
 }
+
+export function updateEmployee(employee) {
+  let updateObj = {};
+
+  Object.keys(employee).forEach((prop) => {
+    console.log(`campo ${prop} tem como valor ${employee[prop]}`);
+
+    // employee[prop] para verificar se tem algum valor
+
+    if (employee[prop] && prop !== "id") {
+      updateObj[prop] = employee[prop];
+    }
+  });
+
+  console.log("Update Object => ", updateObj);
+
+  // employee.name
+  // employee['name']
+
+  return Employee.findByIdAndUpdate(employee.id, updateObj).orFail(() => {
+    throw new Error("Failed to update the Employee");
+  });
+}
+
+export function deleteEmployee(id) {
+  return Employee.findByIdAndDelete(id).orFail(() => {
+    throw new Error("Failed to delete the Employee");
+  });
+}
+
+//clean code
+// when returning an async call, you don't need the async/await declarations
+export function listAllEmployees() {
+  return Employee.find({});
+}
+
+// export async function listAllEmployees() {
+//   const employees = await Employee.find({});
+
+//   return employees;
+// }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 import express from "express";
-import { createEmployee } from "./controllers/employee.js";
+import {
+  createEmployee,
+  deleteEmployee,
+  listAllEmployees,
+  updateEmployee,
+} from "./controllers/employee.js";
 
 const app = express.Router();
 
 app.get("/", (req, res) => {
   res.send("OK"); // Send the response for this route
 });
+
+// route to list all the employees
+
+app.get("/list-employees", async (req, res) => {
+  // const employees = await listAllEmployees();
+  res.send(await listAllEmployees());
+});
+
+// app.get()
 
 app.post("/submit-employee", async (req, res) => {
   const { name, birthDate, gender, state, city, role, salary } = req.body;
@@ -16,4 +30,13 @@ app.post("/submit-employee", async (req, res) => {
   res.send("OK");
 });
 
+app.delete("/delete-employee/:id", async (req, res) => {
+  console.log(req.params);
+  res.send(await deleteEmployee(req.params.id));
+});
+
+app.put("/put-employee", async (req, res) => {
+  console.log(req.body);
+  res.send(await updateEmployee(req.body));
+});
 export default app;
