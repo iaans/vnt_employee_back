47a0a673b1db3d791e7f0be8bc6d5f1550ea6ab4
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-08-05 16:20:06 -0300

    dotenv 3000

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
 import express from "express"; // new method using
 import routes from "./routes.js";
 import { connect } from "./db/connection.js";
 
+dotenv.config();
+
 //const express = require("express"); //Importing express library
 const app = express(); //Create a express app
-const port = 3000; //Define default port for the server to listen || Most used routes: 3000(front), 3333(back), 4000(api)
+const port = process.env.PORT || 3000; //Define default port for the server to listen || Most used routes: 3000(front), 3333(back), 4000(api)
+
+//const port = 3000; //Define default port for the server to listen || Most used routes: 3000(front), 3333(back), 4000(api)
 
 // app.use(cors());
 app.use(express.json());
